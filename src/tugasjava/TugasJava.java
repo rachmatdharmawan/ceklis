70507cb1073df97d1df8540637f18ce64e3c27bc
rachmatdharmawan <dharmawanrachmat515@gmail.com>
2016-11-04 21:37:27 +0700

    12141376 javax.swing.JOptionPane memiliki fungsi menunjukkan bahwa kelas JOptionPane berada di paket swing dan paket swing sendiri mempunyai tempat tersendiri di paket javax.
    public class  memiliki fungsii untuk menjalankian progam program dari dalam dan luar program tersebut.
    public static void main(string[ ] args) adalah fungsi main dimana public pada bagian ini mempunyai arti bahwa metode-metode ini dapat dipanggil didalam maupun diluar kelas.
    static juga berarti sama untuk seluruh instant dari kelas dan
    void sendiri mempunyai arti bahwa metode tidak akan mengirim apapun setelah finish.
    Fungsi main ini terdqapat argument yang diambil dari dalam eksekusi.
    int i, a;merupakan tipe data untuk angka numerik yang tidak menggunakan koma, untuk tipe data 32 bit.

---
+++ src/tugasjava/TugasJava.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugasjava;
+
+/**
+ *
+ * @author AMALIA N A
+ */
+import javax.swing.JOptionPane;
+public class TugasJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int i, j;
+        int panjang;
+        
+        panjang =Integer.valueOf(JOptionPane.showInputDialog("Jumlah Karakter Segitiga:"));
+        System.out.print("Hasil Karakter yang ditampilkan");
